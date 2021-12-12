a90816110b10ca824b614aa4a1793d0264cb7151
Starr <starrleer@gmail.com>
2021-12-12 09:55:40 -0500

    hopefully finished 5...

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,31 +1,70 @@
 package org.launchcode.techjobs.persistent.models;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
-    private String location;
-    @NotBlank
-    private String Location;
+//    private String location;
+//    @NotBlank
+//    private String Location;
 
-    @OneToMany(mappedBy = "employer")
+    @NotBlank(message = "Location is required.")
+    @Size(min = 1, max = 100)
+    public String location;
+
+    //Set onetomany
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+
+//    @OneToMany(mappedBy = "employer")
     private List<Job> jobs = new ArrayList<>();
 
-    public Employer(@NotBlank String location){
+    public Employer() {}
+
+    public Employer(String location) {
+        super();
         this.location = location;
     }
 
-    public Employer () {}
+        //Getters and setters:
 
-    public String getLocation(){
-        return location;
-    }
+        public String getLocation() {
+            return location;
+        }
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
+        public void setLocation(String location) {
+            this.location = location;
+        }
+
+        public List<Job> getJobs() {
+            return jobs;
+        }
+
+        public void setJobs(List<Job> jobs) {
+            this.jobs = jobs;
+        }
+
+//
+//    public Employer(@NotBlank String location){
+//        this.location = location;
+//    }
+//
+//    public Employer () {}
+//
+//    public String getLocation(){
+//        return location;
+//    }
+//
+//    public void setLocation(String location) {
+//        this.location = location;
+//    }
 }
